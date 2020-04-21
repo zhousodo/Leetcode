1589be63e9abca60f5e49d2b31e9ed46d127d7ec
zhousodo <zhousodo@gmail.com>
2020-04-21 14:33:16 +0800

    modify files

--- 教学算法/二叉树.py
+++ 教学算法/二叉树.py
@@ -34,3 +34,25 @@ class Tree(object):
                     queue.append(cur.lchild)
                     queue.append(cur.rchild)
 
+	def preorder(self, root):
+      """递归实现先序遍历"""
+		if root == None:
+			return
+		print(root.elem)
+		self.preorder(root.lchild)
+		self.preorder(root.rchild)
+	
+	def inorder(self, root):
+		"""递归实现中序遍历"""
+		if root == None:
+			return
+		self.inorder(root.lchild)
+		print(root.elem)
+		self.inorder(root.rchild)
+	def postorder(self, root):
+	"""递归实现后续遍历"""
+		if root == None:
+			return
+		self.postorder(root.lchild)
+		self.postorder(root.rchild)
+		print(root.elem)
